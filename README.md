dc755257027a83a74e84c9dfe7651b59e4e92d25
VasudevGupta7 <7vasudevgupta@gmail.com>
2020-05-10 22:07:48 +0530

    dqn implemented for pacman using CNN; colab env render dependencies added

--- README.md
+++ README.md
@@ -2,18 +2,37 @@
 This repo is for playing with reinforcement learning algorithms. I am either using openai gym or ViZDoom as an environment.
 
 
+## Openai gym
 Inorder to install openai gym, run the following command in mac terminal:
 - `pip install gym`
 
-Run the below command in order to use atari environments
+Run the below command in order to use atari environments:
 - `pip install gym[atari]` 
 
-
-Inorder to install ViZDoom, run following commands in mac terminal:
+## ViZDoom
+Inorder to install ViZDoom, run following commands in mac terminal (1st two commands were for installing dependencies for installing ViZDoom):
 - `brew install cmake boost sdl2 wget`
 - `brew cask install julia`
 - `pip install vizdoom`
 
-1st two commands were for installing dependencies for installing ViZDoom
+## Setup- Google Colab
+Inorder to render env in colab, we need to need to install some dependencies; so run following commands in colab:
+- `!apt-get install python-opengl -y`
+- `!apt install xvfb -y`
+- `!pip install pyvirtualdisplay`
+- `!pip install piglet`
+- `!apt-get install x11-utils`
 
+After that run the following code once:
+- `from pyvirtualdisplay import Display`
+- `from IPython import display`
+- `Display().start()`
+
+Then put the following command, just before the episode starts (need to run only once):
+- `img = plt.imshow(env.render('rgb_array'))`
+
+Then put the following code, where we generally put `env.render()`:
+- `img.set_data(env.render('rgb_array'))` # just update the data
+- `display.display(plt.gcf())`
+- `display.clear_output(wait=True)`
 
--- pacman/pacman_dqn.py
+++ pacman/pacman_dqn.py
@@ -11,10 +11,23 @@ import tensorflow as tf
 from collections import deque
 import numpy as np
 import random
+import time
+import matplotlib.pyplot as plt
 
 env= gym.make('MsPacman-v0')
 
-def conv_qnetwork(num_sctions):
+class Agent(object):
+    def __init__(self):
+        self.state_size= (124, 124)
+        self.gamma= 0.9
+        self.replay_memory= deque(maxlen= 10000)
+        self.min_ep= 0.1
+        self.num_actions= env.action_space.n
+        self.qmodel1= self.conv_qnetwork()
+        self.qmodel1.compile(optimizer= tf.keras.optimizers.Adam(LEARNING_RATE), loss= 'mse')
+        self.target_model1= self.conv_qnetwork()
+         
+    def conv_qnetwork(self):
         model= tf.keras.Sequential([
              tf.keras.layers.Conv2D(filters= 8, kernel_size= (3,3), strides= (2,2), padding= "same", input_shape= (124, 124, 3)),
              tf.keras.layers.ReLU(),
@@ -38,77 +51,78 @@ def conv_qnetwork(num_sctions):
              tf.keras.layers.Dense(25),
              tf.keras.layers.ReLU(),
              
-             tf.keras.layers.Dense(num_actions)
+             tf.keras.layers.Dense(self.num_actions)
              ])
         return model
 
-def preprocess_state(init_state, init_state_size):
-    s1= tf.image.resize(init_state, size= init_state_size)
-    s1= tf.expand_dims(s1, 0)
-    return s1    
+    def preprocess_state(self, init_state):
+        s1= tf.image.resize(init_state, size= self.state_size)
+        s1= tf.expand_dims(s1, 0)
+        return s1    
+        
+    def align_network(self):
+        self.target_model1.set_weights(self.qmodel1.get_weights())
+        return
     
-def align_network(target_model, qmodel):
-    target_model.set_weights(qmodel.get_weights())
-    return
-
-def get_action(epsilon, min_ep, s1):
-    # exploration
-    if np.random.random() < epsilon:
-        a1= env.action_space.sample()
-    # exploitation
-    else:
-        qvalues= qmodel(s1)
-        a1= np.argmax(qvalues)
-    
-    if epsilon > min_ep:
-        redn= epsilon/EPISODES
-        epsilon -= redn
-    return a1, epsilon
-
-def train_qmodel(s1, a1, r1, s2, terminated, target_model, qmodel):
-    sample= random.sample(replay_memory, BATCH_SIZE)
-    for s1, a1, r1, s2, terminated in sample:
-        qtarget= qmodel(s1).numpy()
-        if terminated:
-            qtarget[0, a1]= r1
+    def get_action(self, epsilon, s1):
+        # exploration
+        if np.random.random() < epsilon:
+            a1= env.action_space.sample()
+        # exploitation
         else:
-            qtarget[0, a1]= r1 + GAMMA*np.max(target_model(s2))
-        history= qmodel.fit(s1, qtarget, epochs= 1)
-    return history
-
-len_rpm= 10000
-replay_memory= deque(maxlen= len_rpm)
-GAMMA= 0.9
-epsilon= 0.5
-min_ep= 0.1
-LEARNING_RATE= 0.001
-BATCH_SIZE= 3
-init_state_size= (124, 124)
-timesteps= 10
-EPISODES= 2
-num_actions= env.action_space.n
-
-optimizer= tf.keras.optimizers.Adam(LEARNING_RATE)
-qmodel= conv_qnetwork(num_actions)
-qmodel.compile(optimizer, loss= 'mse')
-
-target_model= conv_qnetwork(num_actions)
-
-
-for episode in range(1, EPISODES+1):
-    init_state= env.reset()
-    s1= preprocess_state(init_state, init_state_size)
+            qvalues= self.qmodel1(s1)
+            a1= np.argmax(qvalues)
+        
+        if epsilon > self.min_ep:
+            redn= epsilon/EPISODES
+            epsilon -= redn
+        return a1, epsilon
     
-    for timestep in range(1, timesteps+1):
-        a1, epsilon= get_action(epsilon, min_ep, s1)
-        s2, r1, terminated, info= env.step(a1)
-        s2= preprocess_state(s2, init_state_size)
-        replay_memory.append((s1, a1, r1, s2, terminated))
-        
-        if len(replay_memory) > BATCH_SIZE:
-            history= train_qmodel(s1, a1, r1, s2, terminated, target_model, qmodel)
-        
-        s1= s2
-        if terminated:
-            align_network(target_model, qmodel)
-            break
+    def train_qmodel(self, s1, a1, r1, s2, terminated):
+        sample= random.sample(self.replay_memory, BATCH_SIZE)
+        for s1, a1, r1, s2, terminated in sample:
+            qtarget= self.qmodel1(s1).numpy()
+            if terminated:
+                qtarget[0, a1]= r1
+            else:
+                qtarget[0, a1]= r1 + self.gamma*np.max(self.target_model1(s2))
+            history= self.qmodel1.fit(s1, qtarget, epochs= 1)
+        return history
+
+def episode(agent, timesteps= 100, epsilon= 0.1):
+    start= time.time()
+    avglosses= []
+    for e in range(266, EPISODES+1):
+        init_state= env.reset()
+        s1= agent.preprocess_state(init_state)
+        for timestep in range(1, timesteps+1):
+            #if e >= 1900:
+            env.render()
+            a1, epsilon= agent.get_action(epsilon, s1)
+            s2, r1, terminated, info= env.step(a1)
+            s2= agent.preprocess_state(s2)
+            agent.replay_memory.append((s1, a1, r1, s2, terminated))
+            losses= []
+            if len(agent.replay_memory) > BATCH_SIZE:
+                history= agent.train_qmodel(s1, a1, r1, s2, terminated)
+                losses.append(history.history['loss'])
+            avgloss= np.sum(losses)/ BATCH_SIZE
+            avglosses.append(avgloss)
+            s1= s2
+            if terminated:
+                agent.align_network()
+                break
+        end= time.time()
+        print(f'time taken for training episode-{e} is {end - start}')
+    return avglosses
+
+
+LEARNING_RATE= 0.001
+EPISODES= 500
+BATCH_SIZE= 16
+agent= Agent()
+agent.qmodel1.load_weights('/Users/vasudevgupta/Downloads/checkpoints/checkpoints_pacman_dqn_episodes_266/dqn_pacman')
+avglosses= episode(agent)
+
+#agent.qmodel1.save_weights('/Users/vasudevgupta/Downloads/checkpoints_pacman_dqn/dqn_pacman')
+plt.plot(np.arange(len(avglosses)), avglosses)
